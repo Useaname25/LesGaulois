f76563d4c61fc3aa899194c6662d623cdd12813f
Miora Razanadrakoto <rzm5336a@ensfsi.onmicrosoft.com>
2024-09-19 11:52:57 +0200

    TP1

---
+++ src/personnages/Romain.java
@@ -0,0 +1,19 @@
+package personnages;
+
+public class Romain {
+
+	private String nom;
+	private int force;
+	
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+
+	
+	private String getNom() {
+		return nom;
+	}
+
+}
