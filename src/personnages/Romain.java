27a81ff0e84503024cee4b6e2eb9ae1e6276b40e
Miora Razanadrakoto <rzm5336a@ensfsi.onmicrosoft.com>
2024-09-27 09:40:58 +0200

    fin TP2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,7 +1,6 @@
 package personnages;
 
 public class Romain {
-
 	private String nom;
 	private int force;
 	
@@ -9,11 +8,31 @@ public class Romain {
 		this.nom = nom;
 		this.force = force;
 	}
-
+	
+	
+	public String getNom() {
+		return nom;
+	}
 
 	
-	private String getNom() {
-		return nom;
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+		
+	}
+
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+	 
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+		parler("Aïe");
+		} else {
+		parler("J'abandonne...");
+		}
+		
+		
 	}
 
 }
